6d4c3e54be489f0b2c1d0621f0979050711b6d46
arminsal1 <salehipour1@gmail.com>
2019-03-28 14:50:44 -0400

    Switched to AsyncStorage by react-native-community

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,5 +1,5 @@
 import * as Gun from "gun";
-import { AsyncStorage } from "react-native";
+import AsyncStorage from '@react-native-community/async-storage';
 
 export class Adapter {
   constructor() {
